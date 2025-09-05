5fd40862b12b526cccc13db5ec63c154992b3f6e
srivastavavivek515 <srivastavavivek515@gmail.com>
2025-09-05 18:29:50 +0530

    unit tesing1

---
+++ app/src/main/java/com/altametrics/altametricschedule/ERSUtil.java
@@ -0,0 +1,10 @@
+package com.altametrics.altametricschedule;
+
+/**
+ * @author Altametrics Inc. Created On 05/09/25 6:21 pm
+ */
+public class ERSUtil {
+    public StringBuffer reverseStr(String name){
+        return new StringBuffer(name).reverse();
+    }
+}
---
+++ app/src/test/java/com/altametrics/altametricschedule/ERSUtilTest.java
@@ -0,0 +1,36 @@
+package com.altametrics.altametricschedule;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Altametrics Inc. Created On 05/09/25 6:23 pm
+ */
+
+public class ERSUtilTest extends TestCase {
+
+    private ERSUtil ersUtil;
+
+@Before
+    public void setUp() throws Exception {
+        super.setUp();
+        ersUtil = new ERSUtil();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+        ersUtil = null;
+    }
+
+    @Test
+    public void testReverseStr() {
+    StringBuffer result = ersUtil.reverseStr("vivek");
+    assertEquals("keviv", result.toString());
+    StringBuffer result2 = ersUtil.reverseStr("ankit");
+    assertEquals("keviv", result2.toString());
+    }
+}
